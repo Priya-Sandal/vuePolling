35965d2c08cdc1e7475b7e6cefc4da27f2f17458
Priya-Sandal <priya@innotechteam.in>
2022-11-08 19:04:21 +0530

    add1

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>Signup Form</h1>
+
+    <div id="container">
+        <h1>Signup </h1>
+        <div id="form">
+            <div class="field">
+                <label for="user-name" class="label-field">USERNAME:</label>
+     
+            </div>
+            <div class="field">
+                <label for="user-password">PASSWORD</label>
+            </div>
+            <div class="field">
+                <label for="user-role">ROLE</label>
+            </div>
+        </div>
+    </div>
 </template>
